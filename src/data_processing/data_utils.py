2edebb7016699579526f42aa4a7c9362bd4c1f8a
selaranjeira <serg.laranjeira@gmail.com>
2024-08-07 11:45:16 -0300

    Remove version specification for Conda to fix deployment issue

--- src/data_processing/data_utils.py
+++ src/data_processing/data_utils.py
@@ -5,14 +5,31 @@ def carregar_dados():
     conn = sqlite3.connect('/home/selaranjeira/Desktop/loc_proj/desemp_esportivo/data/runningDB.sqlite3')
     df = pd.read_sql_query("SELECT * FROM running_data", conn)
     conn.close()
+    
+    # tratamento
     df['data'] = pd.to_datetime(df['data'])
     df['quilometragem'] = df['quilometragem'].astype(float)
     
     # Calcular calorias por minuto e km por minuto
     df['calorias_por_minuto'] = df['calorias'] / df['tempo_min']
     df['km_por_minuto'] = df['quilometragem'] / df['tempo_min']
+    df['dia_semana'] = df['data'].dt.day_name()
     
     return df
+
+
+# def carregar_dados():
+#     # Supondo que o arquivo CSV esteja na pasta 'data'
+#     df = pd.read_csv('data/dados.csv', parse_dates=['data'])
+
+#     # Converter a coluna 'data' para o tipo datetime
+#     df['data'] = pd.to_datetime(df['data'])
+
+#     # Adicionar coluna de dia da semana
+#     df['dia_semana'] = df['data'].dt.day_name()
+
+#     return df
+
 
 def calcular_metricas(df):
     # Calcula as métricas desejadas
